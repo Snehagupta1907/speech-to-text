2cf597a716aa07aa1cd2ce560a1ff393f9a9dbb7
Snehagupta1907 <snehagupta98930@gmail.com>
2023-10-31 19:51:21 +0530

    chnages

--- client/src/Components/Content.jsx
+++ client/src/Components/Content.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from 'react';
 import Table from "./Table";
+import Upload from "./Upload";
+
 
 const Content = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const openModal = () => {
+      setModalIsOpen(true);
+    };
+  
+    const closeModal = () => {
+      setModalIsOpen(false);
+    };
   return (
     <div>
       <div className="p-4 sm:ml-64">
@@ -12,34 +23,19 @@ const Content = () => {
               <p>Upload your audio and Video to covert to text</p>
             </div>
             <button
+              data-modal-target="authentication-modal"
+              data-modal-toggle="authentication-modal"
+              class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={openModal}
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
-              Transcribe File
+             Transcribe File
             </button>
+            {/* <Upload isOpen={modalIsOpen} onClose={closeModal} /> */}
           </div>
           <div className="grid grid-cols-3 gap-4 mb-4">
             <div className="flex items-start flex-col justify-center h-32 rounded bg-gray-50 dark:bg-gray-800">
-            <svg
-                className="w-6 h-6 text-gray-800 dark:text-white ml-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m13 19-6-5-6 5V2a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v17Z"
-                />
-              </svg>
-              <p className="text-2xl font-semibold text-white ml-3 mt-3">20</p>
-              <p className="text-2xl font-normal text-white ml-3 mt-3">Saved</p>
-            </div>
-            <div className="flex items-start flex-col justify-center h-32 rounded bg-gray-50 dark:bg-gray-800">
-            <svg
+              <svg
                 className="w-6 h-6 text-gray-800 dark:text-white ml-2"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
@@ -75,7 +71,25 @@ const Content = () => {
               </svg>
               <p className="text-2xl font-semibold text-white ml-3 mt-3">20</p>
               <p className="text-2xl font-normal text-white ml-3 mt-3">Saved</p>
-              
+            </div>
+            <div className="flex items-start flex-col justify-center h-32 rounded bg-gray-50 dark:bg-gray-800">
+              <svg
+                className="w-6 h-6 text-gray-800 dark:text-white ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m13 19-6-5-6 5V2a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v17Z"
+                />
+              </svg>
+              <p className="text-2xl font-semibold text-white ml-3 mt-3">20</p>
+              <p className="text-2xl font-normal text-white ml-3 mt-3">Saved</p>
             </div>
           </div>
           <div className="flex items-center justify-center h-[100vh] mb-4 rounded bg-gray-50 dark:bg-gray-800 mt-10">
@@ -83,6 +97,11 @@ const Content = () => {
           </div>
         </div>
       </div>
+      {modalIsOpen && (
+        
+         <Upload isOpen={modalIsOpen} onClose={closeModal} /> 
+        
+      )}
     </div>
   );
 };
